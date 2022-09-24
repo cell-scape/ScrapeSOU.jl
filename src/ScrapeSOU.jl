609387be41feefa11b543e9ffff1854f03314395
Bradley Dowling <brad.dowling@protonmail.com>
2022-09-24 05:22:19 -0400

    update precomp, index, and new option

--- src/ScrapeSOU.jl
+++ src/ScrapeSOU.jl
@@ -49,6 +49,9 @@ function argparser()
         "--limit", "-l"
             help = "Record limit for Select query"
             arg_type = Int
+        "--local-speeches"
+            help = "Create landing page and local copies of the speeches"
+            action = :store_true
     end
     return s
 end
@@ -78,6 +81,11 @@ function julia_main()::Cint
     try
         @info "Getting list of links..."
         html = scrape(BASE_PATH * LINKS)
+        if args[:local_speeches]
+            @info "Creating local index.html and speeches directory"
+            local_html_links(html, "index.html")
+            return 0
+        end
         if args[:string]
             @info "Scraping speeches and dumping to string..."
             s = scrape_all_to_string(html)
